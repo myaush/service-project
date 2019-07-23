8309cdedc6dac3f6cdfaa1ef1d0b9005481f01b2
andrejlebedev <a.lebedev@imaginweb.ru>
2019-07-23 22:15:19 +0300

    bse

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -43,4 +43,20 @@ $('.repair-info-block__wrapper-list').hide();
 
     });
 
+    var swiperH = new Swiper('.swiper-container-b', {
+        spaceBetween: 50,
+        pagination: {
+            el: '.swiper-pagination-b',
+            clickable: true,
+        },
+    });
+    var swiperV = new Swiper('.swiper-container-v', {
+        direction: 'vertical',
+        spaceBetween: 50,
+        pagination: {
+            el: '.swiper-pagination-v',
+            clickable: true,
+        },
+    });
+
 });
